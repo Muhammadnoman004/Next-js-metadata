f2d2688b989a8a35f48b4ebc03a0acd5f4b75422
Noman Shamim <nomanshamim2004@gmail.com>
2024-03-14 23:56:34 +0500

    All products render

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -5,24 +5,38 @@ export const metadata = {
     description: "All Product Page"
 }
 
-export default function AllProductPage() {
-    let data;
-    const getData = async () => {
-        const Alldata = await fetch('https://dummyjson.com/products')
-            .then(res => res.json())
-            .then(res => {
-                data = res
-                console.log(data);
-            });
+const getData = async () => {
+    return new Promise(async (res, rej) => {
+        const result = await fetch('https://dummyjson.com/products')
+
+        if (result.ok) {
+            const data = await result.json();
+            res(data.products)
+            console.log(data.products);
+        }
+    })
+}
+
+export default async function AllProductPage() {
+    const data = await getData();
+    if (!data) {
+        console.log('data nhi mila...');
     }
-    getData()
     return (
         <div>
-            <h1>All Product Page...</h1>
+            <h1>All Product Page...</h1><br />
             {
-                data.map((singleData) => {
-                    return(
-                        <h2>{singleData}</h2>
+                data.map(singleData => {
+                    return (
+                        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+
+                            <div style={{ border: '2px solid black', padding: '7px', margin: '15px' }}>
+                                <img style={{ width: '300px' }} src={singleData.thumbnail} alt="" />
+                                <h2>{singleData.title}</h2>
+                                <h3>$ {singleData.price}</h3>
+                                <p>{singleData.description}</p><hr />
+                            </div>
+                        </div>
                     )
                 })
             }
